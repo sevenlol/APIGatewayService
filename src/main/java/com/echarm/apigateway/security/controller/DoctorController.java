09589d5ebd9ed7a1216c89233ec4e8618d8fccd2
sevenlol <sevenlol1007@gmail.com>
2015-05-27 00:28:43 +0800

    [master] Add method signature

--- src/main/java/com/echarm/apigateway/security/controller/DoctorController.java
+++ src/main/java/com/echarm/apigateway/security/controller/DoctorController.java
@@ -2,6 +2,8 @@ package com.echarm.apigateway.security.controller;
 
 import java.util.List;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,10 +14,20 @@ import com.echarm.apigateway.accountsystem.model.DoctorAccount;
 @RestController
 public class DoctorController {
 
-	@RequestMapping(value = "/doctors", method = RequestMethod.GET)
+	@RequestMapping(value = "/members/doctors", method = RequestMethod.GET)
     public List<DoctorAccount> getDoctors(@RequestParam(value = "id_list", required = false) String accountIdListStr) {
     	// TODO finish this
     	return null;
     }
+
+	@RequestMapping(value = "/members/doctors", method = RequestMethod.POST)
+	public DoctorAccount registerDoctor(@RequestBody(required=false) DoctorAccount account) {
+		return null;
+	}
+
+	@RequestMapping(value = "/members/doctors", method = RequestMethod.PUT)
+	public DoctorAccount updateDoctor(@RequestBody(required=false) DoctorAccount account, Authentication auth) {
+		return null;
+	}
 
 }
--- src/main/java/com/echarm/apigateway/security/controller/UserController.java
+++ src/main/java/com/echarm/apigateway/security/controller/UserController.java
@@ -1,83 +1,28 @@
 package com.echarm.apigateway.security.controller;
 
-import java.io.IOException;
-import java.security.Principal;
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.social.connect.Connection;
-import org.springframework.social.connect.UserProfile;
-import org.springframework.social.connect.web.ProviderSignInUtils;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.WebRequest;
 
-import com.echarm.apigateway.accountsystem.model.Account;
-import com.echarm.apigateway.security.service.UserDetailsImpl;
+import com.echarm.apigateway.accountsystem.model.UserAccount;
 
 @RestController
 public class UserController {
 
-    @Autowired
-    private ProviderSignInUtils providerSignInUtils;
-
-    /*
-     * if the /user resource is reachable, it returns the currently authenticated user (an Authentication)
-     * else, an 401 response is returned
-     */
-    @RequestMapping("/user")
-    public Account user(Authentication auth) {
-    	if (auth == null)
-    		return null;
-
-    	Object user = auth.getPrincipal();
-    	if (user instanceof UserDetailsImpl) {
-        	Account account = ((UserDetailsImpl) user).getAccount();
-        	if (account != null) {
-        		account.setPassword(null);
-        		account.setSalt(null);
-        	}
-            return account;
-        }
-
-        return null;
-    }
-
-    @RequestMapping(value = "/users", method = RequestMethod.GET)
-    public List<Account> getUsers(@RequestParam(value = "id_list", required = false) String accountIdListStr) {
+	@RequestMapping(value = "/members/users", method = RequestMethod.GET)
+    public List<UserAccount> getUsers(@RequestParam(value = "id_list", required = false) String accountIdListStr) {
     	// TODO finish this
     	return null;
     }
 
-    @RequestMapping("/signup")
-    public void signup(Principal user, WebRequest request, HttpServletResponse httpServletResponse) {
-        Connection<?> connection = providerSignInUtils.getConnectionFromSession(request);
-
-        if (connection != null) {
-            UserProfile socialMediaProfile = connection.fetchUserProfile();
-
-            List<GrantedAuthority> gas = new ArrayList<GrantedAuthority>();
-            gas.add(new SimpleGrantedAuthority("ROLE_USER"));
-            SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken("user", null, gas));
-
-            try {
-                // redirect url
-                httpServletResponse.sendRedirect("/");
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-
-    }
+	@RequestMapping(value = "/members/users", method = RequestMethod.PUT)
+	public UserAccount updateUser(@RequestBody(required=false) UserAccount account, Authentication auth) {
+		// TODO finish this
+		return null;
+	}
 }
