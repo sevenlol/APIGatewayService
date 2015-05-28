feea59575a38ef0cc7bac0b43df9877f7cf82e4e
ian_lin <ian_lin@gemteks.com>
2015-05-28 17:32:17 +0800

    [master] Modify

--- src/main/java/com/echarm/apigateway/security/controller/UserController.java
+++ src/main/java/com/echarm/apigateway/security/controller/UserController.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.echarm.apigateway.accountsystem.error.InvalidParameterException;
+import com.echarm.apigateway.accountsystem.model.Account;
 import com.echarm.apigateway.accountsystem.model.UserAccount;
+import com.echarm.apigateway.accountsystem.util.AccountFieldChecker;
+import com.echarm.apigateway.security.service.UserDetailsImpl;
 import com.echarm.apigateway.security.util.CommaDelimitedStringParser;
 
 @RestController
@@ -32,7 +35,34 @@ public class UserController {
 
 	@RequestMapping(value = "/members/users", method = RequestMethod.PUT)
 	public UserAccount updateUser(@RequestBody(required=false) UserAccount account, Authentication auth) {
-		// TODO finish this
+
+		if (auth == null) {
+			// TODO throw error
+		}
+
+		Object user = auth.getPrincipal();
+		if (!(user instanceof UserDetailsImpl)) {
+			// TODO throw error
+		}
+
+		Account authAccount = ((UserDetailsImpl) user).getAccount();
+		if (authAccount == null) {
+			// TODO throw error
+		}
+
+		// TODO Check Input Account
+		AccountFieldChecker checker = new AccountFieldChecker(AccountFieldChecker.ConnectType.NOT_ALL_FAIL);
+		checker
+			.setChecker(AccountFieldChecker.CheckField.email, AccountFieldChecker.CheckType.BOTH)
+			.setChecker(AccountFieldChecker.CheckField.userInfo, AccountFieldChecker.CheckType.NON_NULL);
+		if (!checker.check(account)) {
+			// TODO check type fail, throw error
+		}
+
+		// TODO set un-updateable fields to null
+
+		// TODO update my account
+
 		return null;
 	}
 }
