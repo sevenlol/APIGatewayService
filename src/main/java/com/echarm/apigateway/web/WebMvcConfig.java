405b8f9cbd50c2e22da603a2e454a375ad51ce95
sevenlol <sevenlol1007@gmail.com>
2015-09-13 22:02:13 +0800

    [master] Route default path to ECharmWebsite/index.html and required files accordingly

--- src/main/java/com/echarm/apigateway/web/WebMvcConfig.java
+++ src/main/java/com/echarm/apigateway/web/WebMvcConfig.java
@@ -1,16 +1,11 @@
 package com.echarm.apigateway.web;
 
-import javax.servlet.Filter;
-
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.resource.GzipResourceResolver;
-import org.springframework.web.servlet.resource.VersionResourceResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 @Configuration
 @AutoConfigureAfter(DispatcherServletAutoConfiguration.class)
@@ -19,22 +14,47 @@ public class WebMvcConfig extends WebMvcAutoConfiguration.WebMvcAutoConfiguratio
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
+       registry
+           .addResourceHandler("/js/**")
+           .addResourceLocations("file:ECharmWebsite/js/");
+
+       registry
+           .addResourceHandler("/css/**")
+           .addResourceLocations("file:ECharmWebsite/css/");
+
+       registry
+           .addResourceHandler("/images/**")
+           .addResourceLocations("file:ECharmWebsite/images/");
+
+       registry
+           .addResourceHandler("/app/**")
+           .addResourceLocations("file:ECharmWebsite/app/");
+
+       registry
+           .addResourceHandler("/fonts/**")
+           .addResourceLocations("file:ECharmWebsite/fonts/");
+
        registry.addResourceHandler("/ECharmWebsite/**")
-//           .addResourceLocations("file:///C:/Users/stephen/Projects/ECharmWebsite/")
-           .addResourceLocations("file:ECharmWebsite/")
-           .setCachePeriod(3600)
-           .resourceChain(true)
-           .addResolver(new GzipResourceResolver())
-           .addResolver(new VersionResourceResolver().addContentVersionStrategy("/**"));
+//           .addResourceLocations("file:///C:/Users/stephen/Projects/ECharmWebsite/");
+           .addResourceLocations("file:ECharmWebsite/");
+//           .setCachePeriod(3600)
+//           .resourceChain(true)
+//           .addResolver(new GzipResourceResolver())
+//           .addResolver(new VersionResourceResolver().addContentVersionStrategy("/**"));
 //           .addTransformer(new AppCacheManifestTransformer());
 
 
        super.addResourceHandlers(registry);
     }
 
-    @Bean
-    public Filter shallowEtagHeaderFilter() {
-      return new ShallowEtagHeaderFilter();
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/ECharmWebsite/index.html");
     }
+
+//    @Bean
+//    public Filter shallowEtagHeaderFilter() {
+//      return new ShallowEtagHeaderFilter();
+//    }
 
 }
