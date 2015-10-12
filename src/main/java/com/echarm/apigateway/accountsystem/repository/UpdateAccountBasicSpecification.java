4b41d9336e5cf67ca5824a6d454149cde1a343f1
sevenlol <sevenlol1007@gmail.com>
2015-10-12 20:43:16 +0800

    [master] Return 409 when attempting to update username/email that already exists

--- src/main/java/com/echarm/apigateway/accountsystem/repository/UpdateAccountBasicSpecification.java
+++ src/main/java/com/echarm/apigateway/accountsystem/repository/UpdateAccountBasicSpecification.java
@@ -9,12 +9,15 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 
+import com.echarm.apigateway.accountsystem.error.AccountExistErrorBody;
 import com.echarm.apigateway.accountsystem.error.AccountNotExistErrorBody;
 import com.echarm.apigateway.accountsystem.error.NoContentException;
+import com.echarm.apigateway.accountsystem.error.ResourceExistException;
 import com.echarm.apigateway.accountsystem.error.ResourceNotExistException;
 import com.echarm.apigateway.accountsystem.error.ServerSideProblemException;
 import com.echarm.apigateway.accountsystem.model.Account;
 import com.echarm.apigateway.accountsystem.model.UserInfo;
+import com.echarm.apigateway.accountsystem.util.UserType;
 
 public class UpdateAccountBasicSpecification extends AccountBasicSpecification {
 
@@ -25,7 +28,7 @@ public class UpdateAccountBasicSpecification extends AccountBasicSpecification {
 
 	@Override
 	Account doActions(MongoTemplate mongoTemplate)
-			throws ResourceNotExistException, NoContentException {
+			throws ResourceNotExistException, NoContentException, ResourceExistException {
 
 		String typeName = account.getUserTypeString();
 		String accountId = account.getAccountId();
@@ -35,6 +38,42 @@ public class UpdateAccountBasicSpecification extends AccountBasicSpecification {
 		}
 		else if(accountId.equals("") || typeName.equals("")) {
 			throw new ServerSideProblemException("Input article model is not complete!!! Missing field: `article_id` or `category`");
+		}
+
+		// attempt to update username or email
+		// check if the to be updated username or email already exists
+		// TODO consider allowing to update to the same email/username (?)
+		if (account.getUserName() != null || account.getEmail() != null) {
+
+		    Query searchQuery = new Query();
+		    searchQuery.addCriteria(Criteria.where("isDeleted").is(false));
+		    if (account.getUserName() != null && account.getEmail() != null) {
+		        searchQuery.addCriteria(Criteria.where("email").exists(true).orOperator(
+		                Criteria.where("userName").is(account.getUserName()),
+		                Criteria.where("email").is(account.getEmail())));
+		    } else {
+		        if (account.getUserName() != null) {
+	                searchQuery.addCriteria(Criteria.where("userName").is(account.getUserName()));
+	            }
+	            if (account.getEmail() != null) {
+	                searchQuery.addCriteria(Criteria.where("email").is(account.getEmail()));
+	            }
+		    }
+
+		    boolean isExist = false;
+		    UserType[] typeArr = UserType.values();
+		    for (UserType type : typeArr) {
+		        if (mongoTemplate.exists(searchQuery, Account.class, type.name())) {
+		            isExist = true;
+		            break;
+		        }
+		    }
+		    if (isExist) {
+		        AccountExistErrorBody eb = new AccountExistErrorBody(account.getUserName(), account.getEmail());
+	            ResourceExistException e = new ResourceExistException("Username or email already exists!");
+	            e.setErrorBody(eb);
+	            throw e;
+		    }
 		}
 
 		String[] fieldNameArr = account.getNonNullCapitalCamelName();
